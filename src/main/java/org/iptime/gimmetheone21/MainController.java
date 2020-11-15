06740f316d5b4d4e6b3175d87583f752b6499421
gimme <gimmetheone124@gmail.com>
2020-11-15 22:45:01 +0900

    FirstCommit

---
+++ src/main/java/org/iptime/gimmetheone21/MainController.java
@@ -0,0 +1,15 @@
+package org.iptime.gimmetheone21;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/")
+    public String test(Model model) {
+        model.addAttribute("msg", "Hello world");
+        return "index";
+    }
+}
